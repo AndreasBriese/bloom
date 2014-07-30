ff2f15901f5628a5c0dc8b44f3e15d2b4c924076
AndreasBriese <ab@edutoolbox.de>
2014-07-30 09:52:53 +0200

    final with test suite

--- bloom.go
+++ bloom.go
@@ -1,34 +1,23 @@
 package bloom
 
 import (
-	"bufio"
 	"bytes"
 	"encoding/json"
 	"hash/fnv"
 	"log"
-	"os"
 )
 
-// Alternative Hash Function
-// http://cr.yp.to/cdb/cdb.txt
-// The cdb hash function is ``h = ((h << 5) + h) ^ c'', with a starting
-// hash of 5381.
-
-// func (bf bloom) djb2(f []byte) uint64 {
-// 	hash := 5381
-// 	for _, c := range f {
-// 		hash = (hash<<5 +hash) ^ int(c)
-// 	}
-// 	return uint64(hash)
-// }
-
+// bloom
+// interface for bloom32 / bloom64 objects
 type bloom interface {
-	Add
-	Has
+	Add([]byte)
+	Has([]byte) bool
+	JSONMarshal() []byte
 }
 
 // getSize
-// helperfunction to set boolset-length (=size of database)
+// helperfunction to calc ceil to the next base 2 power exponent for the bloom boolset-length
+// returns 2**exponent and exponent
 func getSize(ui64 uint64) (size uint64, exponent uint64) {
 	size = 1
 	for size < ui64 {
@@ -38,192 +27,77 @@ func getSize(ui64 uint64) (size uint64, exponent uint64) {
 	return size, exponent
 }
 
-type bloom64 struct {
-	boolSet []bool
-	sizeExp uint64
-	setLocs uint64
-}
-
-func (bf bloom64) fnv64a(b []byte) (l, h uint64) {
-	h64 := fnv.New64a()
-	h64.Write(b)
-	hash64 := h64.Sum64()
-	l = hash64 << 32 >> 32
-	h = hash64 >> 32
-	return l, h
-}
-
-type bloom32 struct {
-	boolSet []bool
-	sizeExp uint64
-	setLocs uint64
-}
-
-// <--- http://www.cse.yorku.ca/~oz/hash.html
-// Berkeley DB Hash (32bit)
-func (bf bloom32) smdb(b []byte) (l, h uint64) {
-	hash := uint32(0)
-	for _, c := range b {
-		hash = uint32(c) + (hash << 6) + (hash << 16) - hash
+// New
+// returns a new bloom32/bloom64 bloomfilter
+func New(entries, locs int) (bloomfilter bloom) {
+	size, exponent := getSize(uint64(entries))
+	switch exponent > 32 {
+	case true:
+		bloomfilter = bloom64{
+			boolSet: make([]bool, size),
+			sizeExp: exponent,
+			size:    size - 1,
+			setLocs: uint64(locs),
+		}
+	case false:
+		bloomfilter = bloom32{
+			boolSet: make([]bool, size),
+			sizeExp: uint32(exponent),
+			size:    uint32(size - 1),
+			setLocs: uint32(locs),
+			shift:   32 - uint32(exponent),
+		}
 	}
-	l = hash << (bf.sizeExp >> 1) >> (bf.sizeExp >> 1)
-	h = hash >> (bf.sizeExp >> 1)
-	return uint64(l), uint64(h)
+	return bloomfilter
 }
 
 // New32
 // returns a new bloom32/bloom64 bloomfilter
-func New(entries, locs int) (bloomfilter bloom) {
-	size, exponent := getSize(uint64(entries))
-	if size > 1<<32 {
-		size, exponent := getSize(uint64(entries))
-		bloomfilter = (bloom64{
-			boolSet: make([]bool, size),
+func NewWithBoolset(bs *[]bool, locs int) (bloomfilter bloom) {
+	size, exponent := getSize(uint64(len(*bs)))
+	switch exponent > 32 {
+	case true:
+		bloomfilter = bloom64{
+			boolSet: *bs,
 			sizeExp: exponent,
+			size:    size - 1,
 			setLocs: uint64(locs),
-		})
+		}
+	case false:
+		bloomfilter = bloom32{
+			boolSet: *bs,
+			sizeExp: uint32(exponent),
+			size:    uint32(size - 1),
+			setLocs: uint32(locs),
+			shift:   32 - uint32(exponent),
+		}
 	}
-	bloomfilter = (bloom32{
-		boolSet: make([]bool, size),
-		sizeExp: exponent,
-		setLocs: uint64(locs)})
 	return bloomfilter
 }
 
-// // New32
-// // returns a new bloom64 filter
-// func New64(entries, locs int) (bloomfilter bloom32) {
-// 	size, exponent := getSize(uint64(entries))
-// 	bloomfilter = (bloom64{
-// 		boolSet: make([]bool, size),
-// 		sizeExp: exponent,
-// 		setLocs: uint64(locs),
-// 	})
-// }
-
-// Empty32
-// returns a new empty bloom32 filter
-func Empty32() (bloomfilter bloom32) {
-	return New(1, 1)
-}
-
-// Empty64
-// returns a new empty bloom64 filter
-func Empty64() (bloomfilter bloom64) {
-	return New(1<<33, 1)
-}
-
-func (bf *bloom32) Add(entry []byte) {
-	l, h := bf.smdb(entry)
-	for i := uint64(0); i < bf.setLocs; i++ {
-		bf.boolSet[(h+i*l)&(1<<bf.sizeExp-1)] = true
-	}
-}
-
-func (bf bloom32) Has(entry []byte) bool {
-	l, h := bf.smdb(entry)
-	for i := uint64(0); i < bf.setLocs; i++ {
-		switch bf.boolSet[(h+i*l)&(1<<bf.sizeExp-1)] {
-		case false:
-			return false
-		}
-	}
-	return true
-}
-
-func (bf *bloom64) Add(entry []byte) {
-	l, h := bf.fnv64a(entry)
-	for i := uint64(0); i < bf.setLocs; i++ {
-		bf.boolSet[(h+i*l)&(1<<bf.sizeExp-1)] = true
-	}
-}
-
-func (bf bloom64) Has(entry []byte) bool {
-	l, h := bf.fnv64a(entry)
-	for i := uint64(0); i < bf.setLocs; i++ {
-		switch bf.boolSet[(h+i*l)&(1<<bf.sizeExp-1)] {
-		case false:
-			return false
-		}
-	}
-	return true
-}
-
-// func (bf bloom32) JSONMarshal() []byte {
-// 	bloomImEx := bloomJSONImExport{}
-// 	bloomImEx.SetLocs = bf.setLocs
-// 	bloomImEx.FilterSet = bf.ToBytes()
-// 	data, err := json.Marshal(bloomImEx)
-// 	if err != nil {
-// 		log.Fatal("json.Marshal failed: ", err)
-// 	}
-// 	return data
-// }
-
-// func (bf *bloom32) JSONUnmarshal(dbData []byte) {
-// 	bloomImEx := bloomJSONImExport{}
-// 	json.Unmarshal(dbData, &bloomImEx)
-// 	buf := bytes.NewBuffer(bloomImEx.FilterSet)
-// 	(*bf).ToBool(buf)
-// 	(*bf).setLocs = bloomImEx.SetLocs
-// 	_, exponent := getSize(uint64(len(bf.boolSet)))
-// 	(*bf).sizeExp = exponent
-// }
-
+// bloomJSONImExport
+// Im/Export structure used by JSONMarshal / JSONUnmarshal
 type bloomJSONImExport struct {
 	FilterSet []byte
 	SetLocs   uint64
 }
 
-func (bf bloom) JSONMarshal() []byte {
-	bloomImEx := bloomJSONImExport{}
-	bloomImEx.SetLocs = bf.setLocs
-	bloomImEx.FilterSet = bf.ToBytes()
-	data, err := json.Marshal(bloomImEx)
-	if err != nil {
-		log.Fatal("json.Marshal failed: ", err)
-	}
-	return data
-}
-
-func (bf *bloom) JSONUnmarshal(dbData []byte) {
+// JSONUnmarshal
+// takes JSON-Object (type bloomJSONImExport) as []bytes
+// returns bloom32 / bloom64 object
+func JSONUnmarshal(dbData []byte) bloom {
 	bloomImEx := bloomJSONImExport{}
 	json.Unmarshal(dbData, &bloomImEx)
 	buf := bytes.NewBuffer(bloomImEx.FilterSet)
-	(*bf).ToBool(buf)
-	(*bf).setLocs = bloomImEx.SetLocs
-	_, exponent := getSize(uint64(len(bf.boolSet)))
-	(*bf).sizeExp = exponent
+	bs := ToBool(buf)
+	bf := NewWithBoolset(&bs, int(bloomImEx.SetLocs))
+	return bf
 }
 
-func (bf *bloom) ToBytes() []byte {
-	var (
-		outBytes bytes.Buffer
-		calc     = []uint8{128, 64, 32, 16, 8, 4, 2, 1}
-		lc       = len(calc)
-		l        = len(bf.boolSet)
-		v        uint8
-		bools    = make([]bool, 8)
-	)
-
-	if l%lc != 0 {
-		log.Fatalf("Len([]bool) must be multiple of %v but is %v !", lc, l)
-	}
-
-	for i := 0; i < l; i += lc {
-		bools = bf.boolSet[i : i+lc]
-		v = uint8(0)
-		for i, b := range bools {
-			if b {
-				v += calc[i]
-			}
-		}
-		outBytes.WriteByte(v)
-	}
-	return outBytes.Bytes()
-}
-
-func (bf *bloom) ToBool(buf *bytes.Buffer) {
+// ToBool
+// takes *bytes.Buffer with bloom.boolset ([]bool) converted to []uint8 by ToBytes()
+// returns []bool
+func ToBool(buf *bytes.Buffer) []bool {
 	var calc = []uint8{128, 64, 32, 16, 8, 4, 2, 1}
 	hlpBool := make([]bool, (8 * (*buf).Len()))
 	index := 0
@@ -241,53 +115,164 @@ func (bf *bloom) ToBool(buf *bytes.Buffer) {
 
 		}
 	}
-	(*bf).boolSet = hlpBool[:index]
+	return hlpBool[:index]
 }
 
-// func (bf *bloom64) ToBytes() []byte {
-// 	var (
-// 		outBytes bytes.Buffer
-// 		calc     = []uint8{128, 64, 32, 16, 8, 4, 2, 1}
-// 		lc       = len(calc)
-// 		l        = len(bf.boolSet)
-// 		v        uint8
-// 		bools    = make([]bool, 8)
-// 	)
+// ToBytes
+// takes pointer to bloom.boolSet (*[]bool) converts to []uint8
+// returns []uint8
+func ToBytes(bf *[]bool) []byte {
+	var (
+		outBytes bytes.Buffer
+		calc     = []uint8{128, 64, 32, 16, 8, 4, 2, 1}
+		lc       = len(calc)
+		l        = len(*bf)
+		v        uint8
+		bools    = make([]bool, 8)
+	)
 
-// 	if l%lc != 0 {
-// 		log.Fatalf("Len([]bool) must be multiple of %v but is %v !", lc, l)
+	if l%lc != 0 {
+		log.Fatalf("Len([]bool) must be multiple of %v but is %v !", lc, l)
+	}
+
+	for i := 0; i < l; i += lc {
+		bools = (*bf)[i : i+lc]
+		v = uint8(0)
+		for i, b := range bools {
+			if b {
+				v += calc[i]
+			}
+		}
+		outBytes.WriteByte(v)
+	}
+	return outBytes.Bytes()
+}
+
+// bloom32
+// bloom filter with max. 2**32 elements
+type bloom32 struct {
+	boolSet []bool
+	sizeExp uint32
+	size    uint32
+	setLocs uint32
+	shift   uint32
+}
+
+// <--- http://www.cse.yorku.ca/~oz/hash.html
+// Berkeley DB Hash (32bit)
+// modified to fit with boolset-length
+// hash is casted to l, h = 16bit fragments
+// returns l,h
+func (bf bloom32) smdb(b []byte) (l, h uint32) {
+	hash := uint32(0)
+	for _, c := range b {
+		hash = uint32(c) + (hash << 6) + (hash << bf.sizeExp) - hash
+	}
+	h = hash >> bf.shift
+	l = hash << bf.sizeExp >> bf.shift
+	return l, h
+}
+
+// Add
+// Add entry to Bloom filter
+func (bf bloom32) Add(entry []byte) {
+	l, h := bf.smdb(entry)
+	for i := uint32(0); i < bf.setLocs; i++ {
+		bf.boolSet[(h+i*l)&bf.size] = true
+	}
+}
+
+// Has
+// Check if entry had been added to Bloom filter
+func (bf bloom32) Has(entry []byte) bool {
+	l, h := bf.smdb(entry)
+	for i := uint32(0); i < bf.setLocs; i++ {
+		switch bf.boolSet[(h+i*l)&bf.size] {
+		case false:
+			return false
+		}
+	}
+	return true
+}
+
+// JSONMarshal
+// returns JSON-object (type bloomJSONImExport) as []byte
+func (bf bloom32) JSONMarshal() []byte {
+	bloomImEx := bloomJSONImExport{}
+	bloomImEx.SetLocs = uint64(bf.setLocs)
+	bloomImEx.FilterSet = ToBytes(&(bf.boolSet))
+	data, err := json.Marshal(bloomImEx)
+	if err != nil {
+		log.Fatal("json.Marshal failed: ", err)
+	}
+	return data
+}
+
+// bloom64
+// bloom filter with max. 2**64 elements
+type bloom64 struct {
+	boolSet []bool
+	sizeExp uint64
+	size    uint64
+	setLocs uint64
+}
+
+// fnv64a
+// use FNV64a Hash to set start (l) and value to be added (h)
+// returns l,h
+func (bf bloom64) fnv64a(b []byte) (l, h uint64) {
+	h64 := fnv.New64a()
+	h64.Write(b)
+	hash64 := h64.Sum64()
+	l = hash64 << 32 >> 32
+	h = hash64 >> 32
+	return l, h
+}
+
+// Add
+// Add entry to Bloom filter
+func (bf bloom64) Add(entry []byte) {
+	l, h := bf.fnv64a(entry)
+	for i := uint64(0); i < bf.setLocs; i++ {
+		bf.boolSet[(h+i*l)&bf.size] = true
+	}
+}
+
+// Has
+// Check if entry had been added to Bloom filter
+func (bf bloom64) Has(entry []byte) bool {
+	l, h := bf.fnv64a(entry)
+	for i := uint64(0); i < bf.setLocs; i++ {
+		switch bf.boolSet[(h+i*l)&bf.size] {
+		case false:
+			return false
+		}
+	}
+	return true
+}
+
+// JSONMarshal
+// returns JSON-object (type bloomJSONImExport) as []byte
+func (bf bloom64) JSONMarshal() []byte {
+	bloomImEx := bloomJSONImExport{}
+	bloomImEx.SetLocs = bf.setLocs
+	bloomImEx.FilterSet = ToBytes(&(bf.boolSet))
+	data, err := json.Marshal(bloomImEx)
+	if err != nil {
+		log.Fatal("json.Marshal failed: ", err)
+	}
+	return data
+}
+
+// Alternative Hash Function
+// http://cr.yp.to/cdb/cdb.txt
+// The cdb hash function is ``h = ((h << 5) + h) ^ c'', with a starting
+// hash of 5381.
+
+// func (bf bloom) djb2(f []byte) uint64 {
+// 	hash := 5381
+// 	for _, c := range f {
+// 		hash = (hash<<5 +hash) ^ int(c)
 // 	}
-
-// 	for i := 0; i < l; i += lc {
-// 		bools = bf.boolSet[i : i+lc]
-// 		v = uint8(0)
-// 		for i, b := range bools {
-// 			if b {
-// 				v += calc[i]
-// 			}
-// 		}
-// 		outBytes.WriteByte(v)
-// 	}
-// 	return outBytes.Bytes()
-// }
-
-// func (bf *bloom64) ToBool(buf *bytes.Buffer) {
-// 	var calc = []uint8{128, 64, 32, 16, 8, 4, 2, 1}
-// 	hlpBool := make([]bool, (8 * (*buf).Len()))
-// 	index := 0
-// 	for {
-// 		if r, err := buf.ReadByte(); err != nil {
-// 			break
-// 		} else {
-// 			for _, c := range calc {
-// 				if r/c > 0 {
-// 					hlpBool[index] = true
-// 					r -= c
-// 				}
-// 				index++
-// 			}
-
-// 		}
-// 	}
-// 	(*bf).boolSet = hlpBool[:index]
+// 	return uint64(hash)
 // }
